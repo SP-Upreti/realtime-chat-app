0adf7252a6d81076b487315d4203b16dc1caac24
SP-Upreti <surendraupreti441@gmail.com>
2024-11-22 11:11:34 +0545

    Update chatbox.jsx

--- client/src/components/chatbox/chatbox.jsx
+++ client/src/components/chatbox/chatbox.jsx
@@ -3,9 +3,10 @@ import Topbar from './topbar'
 import InputBox from './inputBox'
 import Messages from './messages'
 import { AuthContext } from '../../context/authContext'
+import Sidebar from '../sidebar/sidebar'
 
 function Chatbox() {
-    const { selectedUser } = useContext(AuthContext)
+    const { selectedUser, setOpenSidebar, openSidebar } = useContext(AuthContext)
 
     return (
         <div className='w-full relative h-dvh'>
@@ -19,6 +20,9 @@ function Chatbox() {
                 ) :
                     (
                         <div className="hero  min-h-screen">
+                            <span onClick={() => { setOpenSidebar(!Sidebar) }}><svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M4 6H20M4 12H20M4 18H20" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                            </svg></span>
                             <div className="hero-content text-center">
                                 <div className="md:max-w-md">
                                     <h1 className="text-5xl font-bold">Select the user to start convesation</h1>
